a7b3961019b19f901c7203f841065c6eca9c5b14
Vishal Sengar <vishal.sengar@paytmpayments.com>
2024-09-28 23:00:44 +0530

    first commit

---
+++ app/src/main/java/com/example/sampleqrmerchantapp/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.sampleqrmerchantapp
+
+import android.Manifest.permission.CAMERA
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sampleqrmerchantapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    //Camera Runtime Permission Handling Launcher
+    private val requestCameraPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted : Boolean ->
+        if(isGranted) {
+            startCamera()
+        } else {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, CAMERA)) {
+                enablePermissionFromSettings()
+            } else {
+                checkAndRequestCameraPermission()
+            }
+        }
+    }
+
+    //Scanner Activity Result Launcher
+    private val scanResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if(result.resultCode == RESULT_OK) {
+            val qrData = result.data?.getStringExtra("QR_DATA")
+            qrData?.let {
+                Toast.makeText(this, "QR Data: $it", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(this, "Scan cancelled", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.scanButton.setOnClickListener {
+            checkAndRequestCameraPermission()
+        }
+    }
+
+    private fun checkAndRequestCameraPermission() {
+        if(ActivityCompat.checkSelfPermission(this, CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startCamera()
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, CAMERA)) {
+                showPermissionRationale()
+            } else {
+                requestCameraPermissionLauncher.launch(CAMERA)
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val scannerIntent = Intent(this, ScannerActivity::class.java)
+        scanResultLauncher.launch(scannerIntent)
+    }
+
+    private fun showPermissionRationale() {
+        AlertDialog.Builder(this)
+            .setTitle("Camera Permission Needed")
+            .setMessage("Camera access is required to scan QR code. Please grant the permission")
+            .setPositiveButton("Allow") { _,_ -> requestCameraPermissionLauncher.launch(CAMERA)}
+            .setNegativeButton("Deny", null)
+            .create()
+            .show()
+    }
+
+    private fun enablePermissionFromSettings() {
+        AlertDialog.Builder(this)
+            .setTitle("Camera Permission Denied")
+            .setMessage("Camera access is required to scan QR code. You can enable it from settings.")
+            .setPositiveButton("Go to Settings") { _,_ ->
+                val intent = Intent().apply {
+                    action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    data = Uri.fromParts("package", packageName, null)
+                }
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show()
+    }
+
+}
---
+++ app/src/main/java/com/example/sampleqrmerchantapp/ScannerActivity.kt
@@ -0,0 +1,50 @@
+package com.example.sampleqrmerchantapp
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sampleqrmerchantapp.databinding.ActivityScannerBinding
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.DefaultDecoderFactory
+
+class ScannerActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityScannerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityScannerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val formats = listOf(BarcodeFormat.QR_CODE)
+        binding.barcodeScanner.decoderFactory = DefaultDecoderFactory(formats)
+
+        startCamera()
+    }
+
+    private fun startCamera() {
+        binding.barcodeScanner.initializeFromIntent(intent)
+        binding.barcodeScanner.decodeContinuous(callback)
+    }
+
+    private val callback = BarcodeCallback { result ->
+        if (result != null) {
+            // QR Code result is fetched
+            intent.putExtra("QR_DATA", result.text)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.barcodeScanner.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.barcodeScanner.pause()
+    }
+}
